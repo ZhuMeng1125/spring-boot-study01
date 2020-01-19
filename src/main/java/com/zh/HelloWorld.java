64a531af8fb3d7ca83e0abf6b7eaddd19ba20624
ZhuMeng <zhumeng1125@gmail.com>
2020-01-19 21:02:53 +0800

    Initial commit

---
+++ src/main/java/com/zh/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.zh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.figure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        //启动应用
+        SpringApplication.run(HelloWorld.class,args);
+    }
+}
